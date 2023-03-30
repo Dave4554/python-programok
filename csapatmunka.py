f1246c38fa9ec7df235d68b23680edffc201d6e2
Dave4554 <99018834+Dave4554@users.noreply.github.com>
2023-03-30 13:58:36 +0200

    Delete csapatmunka.py

--- csapatmunka.py
+++
@@ -1,7 +0,0 @@
-pcpart = open("pcparts.txt","r",encoding="UTF-8")
-print(pcpart.readline())
-for sorok in pcpart:
-    print(sorok.strip().split("#"))
-
-with open("pcparts.txt","a", encoding="utf-8") as szamok:
-    szamok.write(str(1))
